d9d7e635cbaed8792ebb49051c0aef3f27bdf086
Sravyy <31425335+Sravyy@users.noreply.github.com>
2021-07-16 16:45:37 -0400

    Add files via upload

---
+++ BookCart/Dto/CartItemDTO.cs
@@ -0,0 +1,10 @@
+﻿using BookCart.Models;
+
+namespace BookCart.Dto
+{
+    public class CartItemDTO
+    {
+        public Product Book { get; set; }
+        public int Quantity { get; set; }
+    }
+}
