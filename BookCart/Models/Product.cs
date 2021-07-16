2403fe151a2977e9cf3965069678ad4f8b4e640b
Sravyy <31425335+Sravyy@users.noreply.github.com>
2021-07-16 16:42:54 -0400

    Add files via upload

---
+++ BookCart/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookCart.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string CoverFileName { get; set; }
+    }
+}
