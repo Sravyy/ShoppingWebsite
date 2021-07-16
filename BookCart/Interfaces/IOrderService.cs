ead291c438d7d5af48e348a02977a8f2ed0ae2c4
Sravyy <31425335+Sravyy@users.noreply.github.com>
2021-07-16 16:44:17 -0400

    Add files via upload

---
+++ BookCart/Interfaces/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using BookCart.Dto;
+using System.Collections.Generic;
+
+namespace BookCart.Interfaces
+{
+    public interface IOrderService
+    {
+        void CreateOrder(int userId, OrdersDTO orderDetails);
+        List<OrdersDTO> GetOrderList(int userId);
+    }
+}
---
+++ BookCart/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using BookCart.Dto;
+using BookCart.Models;
+using System.Collections.Generic;
+
+namespace BookCart.Interfaces
+{
+    public interface IProductService
+    {
+        List<Product> GetAllBooks();
+        int AddBook(Product book);
+        int UpdateBook(Product book);
+        Product GetBookData(int bookId);
+        string DeleteBook(int bookId);
+        List<Categories> GetCategories();
+        List<Product> GetSimilarBooks(int bookId);
+        List<CartItemDTO> GetBooksAvailableInCart(string cartId);
+        List<Product> GetBooksAvailableInWishlist(string wishlistID);
+    }
+}
